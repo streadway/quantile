2420d149df857d56c4cecd9afd81a0aa6fc72729
Sean Treadway <sean@soundcloud.com>
2013-05-01 13:22:50 +0200

    Optimization: Handle list operations by hand

--- stats_test.go
+++ stats_test.go
@@ -1,7 +1,6 @@
 package sensor
 
 import (
-	"container/list"
 	"math/rand"
 	"sort"
 	"testing"
@@ -32,8 +31,10 @@ func Test99Percentile(t *testing.T) {
 }
 
 type item struct {
-	v           float64
-	rank, delta int
+	v     float64
+	rank  int
+	delta int
+	next  *item
 }
 
 type quantile struct {
@@ -42,16 +43,17 @@ type quantile struct {
 }
 
 type quantileEstimator struct {
-	samples      *list.List // <rank>
-	targets      []quantile
-	buffer       []float64
+	head         *item
+	items        int
 	observations int
+
+	targets []quantile
+	buffer  []float64
 }
 
 func newBiasedQuantileEstimator(targets ...quantile) *quantileEstimator {
 	return &quantileEstimator{
 		targets: targets,
-		samples: list.New(),
 		buffer:  make([]float64, 0, 500),
 	}
 }
@@ -76,39 +78,54 @@ func (est *quantileEstimator) minError(rank int, n int) int {
 	return int(min)
 }
 
+func (est *quantileEstimator) observe(v float64, rank, delta int, next *item) *item {
+	est.observations++
+	est.items++
+
+	return &item{
+		v:     v,
+		rank:  rank,
+		delta: delta,
+		next:  next,
+	}
+}
+
 // merges the batch
 func (est *quantileEstimator) update(batch []float64) {
-	for _, v := range batch {
-		rank := 0
+	if est.head == nil {
+		est.head = est.observe(batch[0], 1, 0, nil)
+		batch = batch[1:]
+	}
 
-		e := est.samples.Front()
-		if e == nil {
-			e = est.samples.PushFront(&item{v, 1, 0})
-			est.observations++
+	cur := est.head
+	rank := 0
+
+	for _, v := range batch {
+		// min
+		if v < est.head.v {
+			est.head = est.observe(v, 1, 0, est.head)
+			cur = est.head
 			continue
 		}
 
-		for e != nil {
-			cur := e.Value.(*item)
-			if v < cur.v {
-				break
-			}
-			rank = rank + cur.rank
-			e = e.Next()
+		// cursor
+		for cur.next != nil && cur.next.v < v {
+			rank += cur.rank
+			cur = cur.next
 		}
 
-		if e == nil {
-			est.samples.PushBack(&item{v, 1, est.minError(rank, est.observations) - 1})
-		} else {
-			est.samples.InsertBefore(&item{v, 1, est.minError(rank, est.observations) - 1}, e)
+		// max
+		if cur.next == nil {
+			cur.next = est.observe(v, 1, 0, nil)
+			continue
 		}
 
-		est.observations++
+		cur.next = est.observe(v, 1, est.minError(rank, est.observations)-1, cur.next)
 	}
 }
 
 func (est *quantileEstimator) flush() {
-	//sort.Float64Slice(est.buffer).Sort()
+	sort.Float64Slice(est.buffer).Sort()
 	est.update(est.buffer)
 	est.buffer = est.buffer[0:0]
 	est.compress()
@@ -122,53 +139,56 @@ func (est *quantileEstimator) Update(s float64) {
 }
 
 func (est *quantileEstimator) compress() {
-	if est.samples.Len() < 2 {
-		panic("only compress filled samples")
-	}
+	rank := 0
+	cur := est.head
+	for cur != nil && cur.next != nil {
+		if cur.rank+cur.next.rank+cur.next.delta <= est.minError(rank, est.observations) {
+			// merge with previous
+			cur.v = cur.next.v
+			cur.rank += cur.next.rank
+			cur.delta = cur.next.delta
+			cur.next = cur.next.next
 
-	pre := est.samples.Front()
-	cur := pre.Next()
-
-	for rank := 1; cur != nil; pre, cur = cur, cur.Next() {
-		p, c := pre.Value.(*item), cur.Value.(*item)
-		if p.rank+c.rank+c.delta <= est.minError(rank, est.observations) {
-			c.rank += p.rank
-			est.samples.Remove(pre)
-		} else {
-			rank += p.rank
+			est.items--
 		}
-
+		rank += cur.rank
+		cur = cur.next
 	}
 }
 
 func (est *quantileEstimator) Query(q float64) float64 {
+	cur := est.head
+	if cur == nil {
+		return 0
+	}
+
 	est.flush()
 
-	want := int(q * float64(est.observations))
-	rank := 0
-	if pre := est.samples.Front(); pre != nil {
-		cur := pre.Next()
-		for cur != nil {
-			p, c := pre.Value.(*item), cur.Value.(*item)
-			rank += p.rank
-			if rank+c.rank+c.delta > want+est.minError(want, est.observations)/2 {
-				return p.v
-			}
-			pre, cur = cur, cur.Next()
+	quartile := int(q * float64(est.observations))
+	maxrank := quartile + est.minError(quartile, est.observations)/2
+
+	var rank int
+	for cur.next != nil {
+		rank += cur.rank
+		if rank+cur.next.rank+cur.next.delta > maxrank {
+			return cur.v
 		}
-		return est.samples.Back().Value.(*item).v
+		cur = cur.next
 	}
-	return 0
+	return cur.v
+}
+
+func TestQE(t *testing.T) {
+	BenchmarkQuantileEstimator(&testing.B{N: 100000})
 }
 
 func BenchmarkQuantileEstimator(b *testing.B) {
-	est := newBiasedQuantileEstimator(quantile{0.01, 0.01}, quantile{0.05, 0.01}, quantile{0.50, 0.01}, quantile{0.99, 0.001})
-	b.SetBytes(int64(8 * b.N + 2))
-	for i := 0; i < b.N + 2; i++ {
-		est.Update(rand.NormFloat64()*1.0+0.0)
+	est := newBiasedQuantileEstimator(quantile{0.01, 0.001}, quantile{0.05, 0.01}, quantile{0.50, 0.01}, quantile{0.99, 0.001})
+	for i := 0; i < b.N+2; i++ {
+		est.Update(rand.NormFloat64()*1.0 + 0.0)
 	}
 
-	println(est.samples.Len())
+	println(est.items)
 	println(est.Query(0.01))
 	println(est.Query(0.50))
 	println(est.Query(0.99))
